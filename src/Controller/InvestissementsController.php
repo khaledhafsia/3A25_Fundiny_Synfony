a67eaee557b8c4b7e605d6f5d28d4dd2de0562d0
amine1300 <mohammedamine.tej@esprit.tn>
2024-04-28 16:58:56 +0100

    testing payment api

--- src/Controller/InvestissementsController.php
+++ src/Controller/InvestissementsController.php
@@ -11,6 +11,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Stripe\Checkout\Session;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 
 //#[Route('/investissements')]
@@ -218,6 +220,46 @@ class InvestissementsController extends AbstractController
             'investissements' => $investissements,
         ]);
     }
+
+    #[Route('/checkout', name: 'checkout')]
+    public function checkout(): Response
+    {
+        \Stripe\Stripe::setApiKey($_ENV['STRIPE_SECRET_KEY']);
+
+        $session = Session::create([
+            'payment_method_types' => ['card'],
+            'line_items' => [[
+                'price_data' => [
+                    'currency' => 'usd',
+                    'product_data' => [
+                        'name' => 'investissement',
+                    ],
+                    'unit_amount' => 1000, // Amount in cents
+                ],
+                'quantity' => 1,
+            ]],
+            'mode' => 'payment',
+            'success_url' => $this->generateUrl('payment_success', [], UrlGeneratorInterface::ABSOLUTE_URL),
+            'cancel_url' => $this->generateUrl('payment_cancel', [], UrlGeneratorInterface::ABSOLUTE_URL),
+        ]);
+
+        return $this->redirect($session->url);
+    }
+
+
+    #[Route('/payment/success', name: 'payment_success')]
+    public function success(): Response
+    {
+        return $this->redirectToRoute('app_investissements_index');
+    }
+
+    #[Route('/payment/cancel', name: 'payment_cancel')]
+    public function cancel(): Response
+    {
+        return $this->redirectToRoute('app_investissements_new');
+    }
+
+    
     
 
 }
