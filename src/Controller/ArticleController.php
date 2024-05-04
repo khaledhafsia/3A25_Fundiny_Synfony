3cf6cc327aaf42ab6776c18374395afe4f032dae
ayoub <ayoub.warch@esprit.tn>
2024-05-05 00:27:46 +0100

    back and front done

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -20,7 +20,7 @@ use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\JsonResponse;
 
 
-#[Route('/article')]
+//#[Route('/front/article')]
 class ArticleController extends AbstractController
 {
  
@@ -29,9 +29,14 @@ class ArticleController extends AbstractController
     {
         return $this->render('base.html.twig');
     }
+    #[Route('/back', name: 'back')]
+    public function back(): Response
+    {
+        return $this->render('back/baseBack.html.twig');
+    }
 
  
-    #[Route('/', name: 'app_article_index', methods: ['GET'])]
+    #[Route('/front/article', name: 'app_article_index', methods: ['GET'])]
     public function index(ArticleRepository $articleRepository, CommentRepository $commentRepository): Response
     {
         // Get all articles
@@ -51,13 +56,13 @@ class ArticleController extends AbstractController
         $commentForm = $this->createForm(CommentType::class, $newComment);
     
         // Render the Twig template with articles, comments, and the comment form
-        return $this->render('article/index.html.twig', [
+        return $this->render('front/article/index.html.twig', [
             'articles' => $articles,
             'articleComments' => $articleComments,
             'commentForm' => $commentForm->createView(),
         ]);
     }
-    #[Route('/new', name: 'app_article_new', methods: ['GET', 'POST'])]
+    #[Route('/front/article/new', name: 'app_article_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $article = new Article();
@@ -90,13 +95,13 @@ class ArticleController extends AbstractController
             return $this->redirectToRoute('app_article_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('article/new.html.twig', [
+        return $this->renderForm('front/article/new.html.twig', [
             'article' => $article,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_article_edit', methods: ['GET', 'POST'])]
+    #[Route('/front/article/{id}/edit', name: 'app_article_edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Article $article, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $form = $this->createForm(ArticleType::class, $article);
@@ -127,21 +132,21 @@ class ArticleController extends AbstractController
             return $this->redirectToRoute('app_article_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('article/edit.html.twig', [
+        return $this->renderForm('front/article/edit.html.twig', [
             'article' => $article,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_article_show', methods: ['GET'])]
+    #[Route('/front/article/{id}', name: 'app_article_show', methods: ['GET'])]
     public function show(Article $article): Response
     {
-        return $this->render('article/show.html.twig', [
+        return $this->render('front/article/show.html.twig', [
             'article' => $article,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_article_delete', methods: ['POST'])]
+    #[Route('/front/article/{id}', name: 'app_article_delete', methods: ['POST'])]
     public function delete(Request $request, Article $article, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
@@ -151,7 +156,7 @@ class ArticleController extends AbstractController
 
         return $this->redirectToRoute('app_article_index', [], Response::HTTP_SEE_OTHER);
     }
-    #[Route('/search', name: 'app_article_search', methods: ['GET','POST'])]
+    #[Route('front/article/search', name: 'app_article_search', methods: ['GET','POST'])]
 public function search(Request $request, ArticleRepository $articleRepository): Response
 {
     $searchTerm = $request->query->get('search');
@@ -173,12 +178,159 @@ public function search(Request $request, ArticleRepository $articleRepository): 
     $newComment = new Comment();
     $commentForm = $this->createForm(CommentType::class, $newComment);
 
-    return $this->render('article/index.html.twig', [
+    return $this->render('front/article/index.html.twig', [
         'articles' => $articles,
         'articleComments' => $articleComments,
         'commentForm' => $commentForm->createView(),
     ]);
 }
+#[Route('/back/article', name: 'app_articleback_index', methods: ['GET'])]
+public function indexback(ArticleRepository $articleRepository, CommentRepository $commentRepository): Response
+{
+    // Get all articles
+    $articles = $articleRepository->findAll();
 
+    // Create an associative array to store comments for each article
+    $articleComments = [];
+
+    // Loop through each article and fetch its associated comments
+    foreach ($articles as $article) {
+        $comments = $commentRepository->findBy(['postid' => $article->getId()]);
+        $articleComments[$article->getId()] = $comments;
+    }
+
+    // Create a new comment object for the comment form
+    $newComment = new Comment();
+    $commentForm = $this->createForm(CommentType::class, $newComment);
+
+    // Render the Twig template with articles, comments, and the comment form
+    return $this->render('back/article/index.html.twig', [
+        'articles' => $articles,
+        'articleComments' => $articleComments,
+        'commentForm' => $commentForm->createView(),
+    ]);
+}
+#[Route('/back/article/new', name: 'app_articleback_new', methods: ['GET', 'POST'])]
+public function newback(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+{
+    $article = new Article();
+    $form = $this->createForm(ArticleType::class, $article);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $imageFile = $form->get('imageFile')->getData();
+
+        if ($imageFile) {
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+            try {
+                $imageFile->move(
+                    $this->getParameter('kernel.project_dir') . '/public/images/front',
+                    $newFilename
+                );
+            } catch (SQLite3Exception  ) {
+                printf("error");
+            }
+
+            $article->setImage($newFilename);
+        }
+
+        $entityManager->persist($article);
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_articleback_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    return $this->renderForm('back/article/new.html.twig', [
+        'article' => $article,
+        'form' => $form,
+    ]);
+}
+
+#[Route('/back/article/{id}/edit', name: 'app_articleback_edit', methods: ['GET', 'POST'])]
+public function editback(Request $request, Article $article, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+{
+    $form = $this->createForm(ArticleType::class, $article);
+    $form->handleRequest($request);
+
+    if ($form->isSubmitted() && $form->isValid()) {
+        $imageFile = $form->get('imageFile')->getData();
+
+        if ($imageFile) {
+            $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
+            $safeFilename = $slugger->slug($originalFilename);
+            $newFilename = $safeFilename.'-'.uniqid().'.'.$imageFile->guessExtension();
+
+            try {
+                $imageFile->move(
+                    $this->getParameter('kernel.project_dir') . '/public/images/front',
+                    $newFilename
+                );
+            } catch (SQLite3Exception ) {
+                // Handle file upload error
+            }
+
+            $article->setImage($newFilename);
+        }
+
+        $entityManager->flush();
+
+        return $this->redirectToRoute('app_articleback_index', [], Response::HTTP_SEE_OTHER);
+    }
+
+    return $this->renderForm('back/article/edit.html.twig', [
+        'article' => $article,
+        'form' => $form,
+    ]);
+}
+
+#[Route('/back/article/{id}', name: 'app_articleback_show', methods: ['GET'])]
+public function showback(Article $article): Response
+{
+    return $this->render('back/article/show.html.twig', [
+        'article' => $article,
+    ]);
+}
+
+#[Route('/front/article/{id}', name: 'app_articleback_delete', methods: ['POST'])]
+public function deleteback(Request $request, Article $article, EntityManagerInterface $entityManager): Response
+{
+    if ($this->isCsrfTokenValid('delete'.$article->getId(), $request->request->get('_token'))) {
+        $entityManager->remove($article);
+        $entityManager->flush();
+    }
+
+    return $this->redirectToRoute('app_articleback_index', [], Response::HTTP_SEE_OTHER);
+}
+#[Route('back/article/search', name: 'app_articleback_search', methods: ['GET','POST'])]
+public function searchback(Request $request, ArticleRepository $articleRepository): Response
+{
+$searchTerm = $request->query->get('search');
+
+// Fetch articles based on the search term
+$articles = $articleRepository->findByDescription($searchTerm);
+
+// Create an associative array to store comments for each article
+$articleComments = [];
+$commentRepository = $this->getDoctrine()->getRepository(Comment::class);
+
+// Loop through each fetched article and fetch its associated comments
+foreach ($articles as $article) {
+    $comments = $commentRepository->findBy(['article' => $article]);
+    $articleComments[$article->getId()] = $comments;
+}
+
+// Create a new comment object for the comment form
+$newComment = new Comment();
+$commentForm = $this->createForm(CommentType::class, $newComment);
+
+return $this->render('back/article/index.html.twig', [
+    'articles' => $articles,
+    'articleComments' => $articleComments,
+    'commentForm' => $commentForm->createView(),
+]);
+}
 
 }
--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -13,7 +13,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 
-#[Route('/comment')]
+//#[Route('/comment')]
 class CommentController extends AbstractController
 {
     #[Route('/', name: 'app_comment_index', methods: ['GET'])]
@@ -107,5 +107,97 @@ public function new(Request $request): Response
 
         // Redirect or return a response as needed
         return $this->redirectToRoute('app_article_index'); // Example redirect
+    }
+    #[Route('/back/comment', name: 'app_commentback_index', methods: ['GET'])]
+    public function indexback(CommentRepository $commentRepository): Response
+    {
+        return $this->render('front/comment/index.html.twig', [
+            'comments' => $commentRepository->findAll(),
+        ]);
+    }
+
+    #[Route('back/comment/new', name: 'app_commentback_new', methods: ['POST'])]
+public function newback(Request $request): Response
+{
+    $commentText = $request->request->get('comment');
+    $articleId = $request->request->get('articleId');
+
+    // Fetch the Article entity based on the ID
+    $article = $this->getDoctrine()->getRepository(Article::class)->find($articleId);
+
+    if (!$article) {
+        throw $this->createNotFoundException('Article not found');
+    }
+
+    // Create a new Comment object
+    $comment = new Comment();
+    $comment->setComment($commentText);
+    $comment->setPostid($article);
+
+    // Get the entity manager
+    $entityManager = $this->getDoctrine()->getManager();
+
+    // Persist and flush the new comment
+    $entityManager->persist($comment);
+    $entityManager->flush();
+
+    // Redirect back to the index page after adding the comment
+    return $this->redirectToRoute('app_articleback_index');
+}
+    
+    #[Route('back/comment/{commentid}/edit', name: 'app_commentback_edit', methods: ['GET', 'POST'])]
+    public function editback(Request $request, int $commentid): Response
+    {
+        // Get the comment entity from the database
+        $entityManager = $this->getDoctrine()->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($commentid);
+
+        // Check if the comment exists
+        if (!$comment) {
+            throw $this->createNotFoundException('Comment not found');
+        }
+
+        // Create the comment edit form
+        $form = $this->createForm(CommentType::class, $comment, [
+            'action' => $this->generateUrl('app_commentback_edit', ['commentid' => $commentid]),
+            'method' => 'POST',
+        ]);
+
+        // Handle form submission
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Save the edited comment
+            $entityManager->persist($comment);
+            $entityManager->flush();
+
+            // Redirect or return a response as needed
+            return $this->redirectToRoute('app_articleback_index'); // Example redirect
+        }
+
+        // Render the edit comment form
+        return $this->render('back/comment/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    
+}
+    #[Route('back/comment/{commentid}', name: 'app_commentback_delete', methods: ['POST'])] 
+        public function deleteback(Request $request, int $commentid): Response
+    {
+        // Get the comment entity from the database
+        $entityManager = $this->getDoctrine()->getManager();
+        $comment = $entityManager->getRepository(Comment::class)->find($commentid);
+
+        // Check if the comment exists
+        if (!$comment) {
+            throw $this->createNotFoundException('Comment not found');
+        }
+
+        // Handle comment deletion logic
+        $entityManager->remove($comment);
+        $entityManager->flush();
+
+        // Redirect or return a response as needed
+        return $this->redirectToRoute('app_articleback_index'); // Example redirect
     }
 }
